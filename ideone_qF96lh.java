6ab4b7bc5c4b351eadaa4ee415bdfa1ab505d9e7
Yonathan-Rios <156020384+Yonathan-Rios@users.noreply.github.com>
2024-01-08 14:56:22 -0600

    Mensaje privado

--- ideone_qF96lh.java
+++ ideone_qF96lh.java
@@ -1,14 +1,31 @@
-/* package whatever; // don't place package name! */
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <title>Página de Yoni</title>
+</head>
+<body>
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+  <h1>¡Bienvenido a la página de Yoni!</h1>
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-	}
-}
+  <form action="index.php" method="post">
+    <input type="text" name="nombre" placeholder="Tu nombre">
+    <input type="radio" name="conoce_a_yoni" value="si"> Sí
+    <input type="radio" name="conoce_a_yoni" value="no"> No
+    <input type="submit" value="Siguiente">
+  </form>
+
+  <?php
+
+    if ($_POST["conoce_a_yoni"] == "si") {
+      echo "<h2>Opción 1</h2>";
+      echo "<p>Vale por una salida a comer y Recorcholis o Happyland</p>";
+      echo "<a href='index.php?opcion=2'>Quieres saber que tenía la opción 2?</a>";
+    } else {
+      echo "<h2>Opción 2</h2>";
+      echo "<p>Vale para ir a desayunar a un lugar bonito y cine (esto puede cambiar)</p>";
+    }
+
+  ?>
+
+</body>
+</html>
